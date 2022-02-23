2c2a0948adc9d8b8bbb2644777f9d3cd1491c5ce
Pedro Escaleira <escaleira@av.it.pt>
2022-02-23 12:02:22 +0000

    charm creation script fixed git clone; k8s_controller_installer base charm created

--- charm_create.sh
+++ charm_create.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 
-# Charm creation, with the commands and files' contents examples given by the Day
-# 1: VNF Services Initialization OSM Documentation
+# Charm creation, adapted from the commands and files' contents examples given
+# by the Day 1: VNF Services Initialization OSM Documentation
 # (https://osm.etsi.org/docs/vnf-onboarding-guidelines/03-day1.html)
 
 
@@ -27,8 +27,8 @@ chmod +x src/charm.py
 ln -s ../src/charm.py hooks/upgrade-charm
 ln -s ../src/charm.py hooks/install
 ln -s ../src/charm.py hooks/start
-git clone https://github.com/canonical/operator mod/operator
-git clone https://github.com/charmed-osm/charms.osm mod/charms.osm
+git submodule add https://github.com/canonical/operator mod/operator
+git submodule add https://github.com/charmed-osm/charms.osm mod/charms.osm
 ln -s ../mod/operator/ops lib/ops
 ln -s ../mod/charms.osm/charms lib/charms
 
