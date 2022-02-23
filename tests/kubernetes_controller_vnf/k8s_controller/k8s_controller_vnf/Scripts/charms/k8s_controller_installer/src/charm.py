801ac1c2f789071cb730ee5d49f86cd868adef5d
Pedro Escaleira <escaleira@av.it.pt>
2022-02-23 21:07:48 +0000

    more k8s controller charm commands done

--- tests/kubernetes_controller_vnf/k8s_controller/k8s_controller_vnf/Scripts/charms/k8s_controller_installer/src/charm.py
+++ tests/kubernetes_controller_vnf/k8s_controller/k8s_controller_vnf/Scripts/charms/k8s_controller_installer/src/charm.py
@@ -147,6 +147,204 @@ class SampleProxyCharm(SSHProxyCharm):
 		proxy = self.get_ssh_proxy()
 		return commands.unit_run_command(logger=logger, proxy=proxy, unit_status=self.unit.status)
 
+	def __disable_swap(self, event) -> bool:
+		commands = Commands()
+
+		# Add the Kubernetes repository
+		commands.add_command(Command(
+			cmd="""sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab""",
+			initial_status="Updating system's package information...",
+			ok_status="System packages' information updated",
+			error_status="Couldn't update systems packages' information"
+		))
+		commands.add_command(Command(
+			cmd="sudo swapoff -a",
+			initial_status="Installing curl and apt-transport-https packages...",
+			ok_status="Installed curl and apt-transport-https",
+			error_status="Couldn't install curl and apt-transport-https"
+		))
+
+		commands.add_command(Command(
+			cmd="sudo modprobe overlay",
+			initial_status="Adding GPG Google key...",
+			ok_status="Added GPG Google key",
+			error_status="Couldn't add GPG Google key"
+		))
+		commands.add_command(Command(
+			cmd="sudo modprobe br_netfilter",
+			initial_status="Adding Kubernetes repository...",
+			ok_status="Kubernetes repository added",
+			error_status="Couldn't add Kubernetes repository"
+		))
+
+		#
+		commands.add_command(Command(
+			cmd="""sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+			net.bridge.bridge-nf-call-ip6tables = 1
+			net.bridge.bridge-nf-call-iptables = 1
+			net.ipv4.ip_forward = 1
+			EOF""",
+			initial_status="Updating system's package information...",
+			ok_status="System packages' information updated",
+			error_status="Couldn't update systems packages' information"
+		))
+
+
+		commands.add_command(Command(
+			cmd="sudo sysctl --system",
+			initial_status="Installing the kubelet, kubeadm and kubectl packages...",
+			ok_status="Kubelet, kubeadm and kubectl packages installed",
+			error_status="Couldn't install kubelet, kubeadm and kubectl packages"
+		))
+
+		proxy = self.get_ssh_proxy()
+		return commands.unit_run_command(logger=logger, proxy=proxy, unit_status=self.unit.status)
+
+	def __install_container_runtime(self):
+		# Containerd
+		commands = Commands()
+
+		# Configure persistent loading of modules
+		commands.add_command(Command(
+			cmd="""sudo tee /etc/modules-load.d/containerd.conf <<EOF
+			overlay
+			br_netfilter
+			EOF""",
+			initial_status="Updating system's package information...",
+			ok_status="System packages' information updated",
+			error_status="Couldn't update systems packages' information"
+		))
+
+		# Load at runtime
+		commands.add_command(Command(
+			cmd="sudo modprobe overlay",
+			initial_status="Installing curl and apt-transport-https packages...",
+			ok_status="Installed curl and apt-transport-https",
+			error_status="Couldn't install curl and apt-transport-https"
+		))
+		commands.add_command(Command(
+			cmd="sudo modprobe br_netfilter",
+			initial_status="Adding GPG Google key...",
+			ok_status="Added GPG Google key",
+			error_status="Couldn't add GPG Google key"
+		))
+
+		# Ensure sysctl params are set
+		commands.add_command(Command(
+			cmd="""sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+			net.bridge.bridge-nf-call-ip6tables = 1
+			net.bridge.bridge-nf-call-iptables = 1
+			net.ipv4.ip_forward = 1
+			EOF""",
+			initial_status="Adding Kubernetes repository...",
+			ok_status="Kubernetes repository added",
+			error_status="Couldn't add Kubernetes repository"
+		))
+
+		# Reload configs
+		commands.add_command(Command(
+			cmd="sudo sysctl --system",
+			initial_status="Updating system's package information...",
+			ok_status="System packages' information updated",
+			error_status="Couldn't update systems packages' information"
+		))
+
+		# Install required packages
+		commands.add_command(Command(
+			cmd="sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates",
+			initial_status="Installing the kubelet, kubeadm and kubectl packages...",
+			ok_status="Kubelet, kubeadm and kubectl packages installed",
+			error_status="Couldn't install kubelet, kubeadm and kubectl packages"
+		))
+
+		# Add Docker repo
+		commands.add_command(Command(
+			cmd="curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+			initial_status="Installing the kubelet, kubeadm and kubectl packages...",
+			ok_status="Kubelet, kubeadm and kubectl packages installed",
+			error_status="Couldn't install kubelet, kubeadm and kubectl packages"
+		))
+		commands.add_command(Command(
+			cmd="""sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) 
+			stable""",
+			initial_status="Installing the kubelet, kubeadm and kubectl packages...",
+			ok_status="Kubelet, kubeadm and kubectl packages installed",
+			error_status="Couldn't install kubelet, kubeadm and kubectl packages"
+		))
+
+		# Install containerd
+		commands.add_command(Command(
+			cmd="sudo apt update",
+			initial_status="Installing the kubelet, kubeadm and kubectl packages...",
+			ok_status="Kubelet, kubeadm and kubectl packages installed",
+			error_status="Couldn't install kubelet, kubeadm and kubectl packages"
+		))
+		commands.add_command(Command(
+			cmd="sudo apt install -y containerd.io",
+			initial_status="Installing the kubelet, kubeadm and kubectl packages...",
+			ok_status="Kubelet, kubeadm and kubectl packages installed",
+			error_status="Couldn't install kubelet, kubeadm and kubectl packages"
+		))
+
+		# Install containerd
+		commands.add_command(Command(
+			cmd="sudo mkdir -p /etc/containerd",
+			initial_status="Installing the kubelet, kubeadm and kubectl packages...",
+			ok_status="Kubelet, kubeadm and kubectl packages installed",
+			error_status="Couldn't install kubelet, kubeadm and kubectl packages"
+		))
+		commands.add_command(Command(
+			cmd="sudo containerd config default>/etc/containerd/config.toml",
+			initial_status="Installing the kubelet, kubeadm and kubectl packages...",
+			ok_status="Kubelet, kubeadm and kubectl packages installed",
+			error_status="Couldn't install kubelet, kubeadm and kubectl packages"
+		))
+
+		# Restart containerd
+		commands.add_command(Command(
+			cmd="sudo systemctl restart containerd",
+			initial_status="Installing the kubelet, kubeadm and kubectl packages...",
+			ok_status="Kubelet, kubeadm and kubectl packages installed",
+			error_status="Couldn't install kubelet, kubeadm and kubectl packages"
+		))
+		commands.add_command(Command(
+			cmd="sudo systemctl enable containerd",
+			initial_status="Installing the kubelet, kubeadm and kubectl packages...",
+			ok_status="Kubelet, kubeadm and kubectl packages installed",
+			error_status="Couldn't install kubelet, kubeadm and kubectl packages"
+		))
+		commands.add_command(Command(
+			cmd="systemctl status  containerd",
+			initial_status="Installing the kubelet, kubeadm and kubectl packages...",
+			ok_status="Kubelet, kubeadm and kubectl packages installed",
+			error_status="Couldn't install kubelet, kubeadm and kubectl packages"
+		))
+
+		proxy = self.get_ssh_proxy()
+		return commands.unit_run_command(logger=logger, proxy=proxy, unit_status=self.unit.status)
+
+	def __initialize_master_node(self):
+		commands = Commands()
+
+		# Enable kubelet service
+		commands.add_command(Command(
+			cmd="sudo systemctl enable kubelet",
+			initial_status="Updating system's package information...",
+			ok_status="System packages' information updated",
+			error_status="Couldn't update systems packages' information"
+		))
+
+		# Pull container images
+		commands.add_command(Command(
+			cmd="sudo kubeadm config images pull",
+			initial_status="Updating system's package information...",
+			ok_status="System packages' information updated",
+			error_status="Couldn't update systems packages' information"
+		))
+
+		proxy = self.get_ssh_proxy()
+		return commands.unit_run_command(logger=logger, proxy=proxy, unit_status=self.unit.status)
+
 
 if __name__ == "__main__":
 	main(SampleProxyCharm)
