f261c3b60651ac240042bf2a9f3a04b9c483187b
MotaMiguel <miguel.mota@av.it.pt>
2022-05-06 17:14:21 +0100

    Altered dumpparse to better fit the it's purpose
    Signed-off-by: MotaMiguel <miguel.mota@av.it.pt>

--- results/dumpparse.py
+++ results/dumpparse.py
@@ -11,8 +11,8 @@ def read_ns(dir):
         tmp_data = "\n".join(file.readlines()).split("Received message")
     # remove index 0 which is ''
     tmp_data.pop(0)
-    ns_data = ["instance_start","instance_started","delta"]
-    ns_values = []
+    ns_data = ["iteration","instance_start","instance_started","delta"]
+    ns_values = [dir.split("/")[0].split("_")[-1]]
     for data in tmp_data:
         if not re.match("^ dummy message.*",data):
             # get the key value 
@@ -31,13 +31,31 @@ def read_ns(dir):
                     ns_values.append(instantiated_time-instantiate_start_time)
     
     # loop indexes for csv write and zip
-    return [("file-name",dir)]+list(zip([ns_data * len(ns_values)][0][:len(ns_values)],ns_values))
-    
+    ns_fields = [ns_data[0]]+[(ns_data[1:] * len(ns_values))][0][:len(ns_values)-1]
+    # update the name
+    ns_fields = update_name(ns_fields)
+    ret = dict(zip(ns_fields,ns_values))
+    ret["fields"] = ns_fields
+    return ret
+
+def update_name(data):
+    # transforms the data to 
+    counts = {}
+    for index_name in range(len(data)):
+        for index_same_name in range(1,len(data)):
+            if data[index_name]==data[index_same_name]:
+                if data[index_name] in counts:
+                    counts[data[index_name]]+=1
+                else:
+                    counts[data[index_name]]=0
+                data[index_same_name] = data[index_same_name]+str(counts[data[index_same_name]])
+    return data
+            
 def read_mep(dir):
     # TODO MISSING INITIALIZED TIME FROM NS
     # Order of each mep received action
-    mep_data = ["mep_initializaton","mec_app_ready","mec_app_service_creation","mec_app_subscriptions","mec_app_service_query"]
-    mep_values = []
+    mep_data = ["iteration","mep_initializaton","mec_app_ready","mec_app_service_creation","mec_app_subscriptions","mec_app_service_query"]
+    mep_values = [dir.split("/")[0].split("_")[-1]]
     with open(dir,"r") as file:
         line = file.readline()
         while line:
@@ -47,7 +65,9 @@ def read_mep(dir):
             mep_values.append(int(line.split(" ")[-1])/1000)
             line = file.readline()
     # return a dict for CSV write for usage in graphical analysis 
-    return [("file-name",dir)]+list(zip(mep_data,mep_values))
+    ret = dict(zip(mep_data,mep_values))
+    ret["fields"] = mep_data 
+    return ret
 
 def pod_dump(dir,main_ns_data,charmed_ns_data,mep_data):
     # iterate all known pod dirs and their files
@@ -63,11 +83,13 @@ def osm_yaml(dir):
 
 def write_parsed_data(file_name,data):
     with open(file_name,"w+") as outfile:
-        writer = csv.writer(outfile)
+        firstLine = True
         for iteration in data:
-            writer.writerows(iteration)
-            writer.writerow("")
-
+            fields = iteration.pop("fields")
+            if firstLine:
+                outfile.write(",".join(fields)+"\n")
+                firstLine = False
+            outfile.write(",".join([str(val) for val in iteration.values()])+"\n")
 def main():
     # Iterate dump directories
     # Using re and dirlisting to avoid having to know how many dumps there are
@@ -78,7 +100,11 @@ def main():
         if re.match("^results.*",dumpdir):
             # enter directory and iterate known dumps
             pod_dump(dumpdir,main_ns_data,charmed_ns_data,mep_data)
-            
+    
+    """print(main_ns_data)
+    print(charmed_ns_data)
+    print(mep_data)"""
+    
     write_parsed_data("main_ns_data",main_ns_data)
     write_parsed_data("charmed_ns_data",charmed_ns_data)
     write_parsed_data("mep_data",mep_data)
